f5cedbb5bc299ce8a885a06071ac540a605fd015
tcdw <tcdw2011@gmail.com>
2024-09-06 15:08:05 +0800

    attempt to upgrade source

---
+++ source/translate/translate.bat
@@ -0,0 +1,5 @@
+@ECHO OFF
+REM %LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js minifyall
+%LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js translateall
+%LOCALAPPDATA%\..\Roaming\nvm\v12.13.0\node translate.js extractall
+pause
